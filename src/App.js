60ba30565fad83add4f4fe8b7a0c478fbdede2e2
Victoria Green <victoria.cousan@gmail.com>
2021-08-25 17:57:17 -0500

    changes to landing page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      Home page
-    <br/><br/>
       THIS IS THE LANDING PAGE I GUESS!
+      <br/><br/>
+      <h1>Things needed on page</h1>
+      <ul>
+      <li>Description of you application </li>
+      <li>Explain your project's features</li>
+      <li>Include a short demo video  </li>
+      <li>Tutorial on how to use or interact with your application </li>
+      <li>Obvious and clear button to link to your live application </li>
+      <li>"Meet the Engineers" that shows a photo of the you and your team plus links to your LinkedIn, Twitter, and Github</li>
+      </ul>
+    
     </>
   )
 }
